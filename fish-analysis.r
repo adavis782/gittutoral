0ecf2a0a8e59ebf683b2857dfe31f4836ffb4fd5
Alexa Davis <adavis31@unl.edu>
2018-10-19 11:33:31 -0500

    changed length

--- fish-analysis.r
+++ fish-analysis.r
@@ -4,6 +4,6 @@ library(tidyverse)
 data<-read.csv("D:/NRES800/2018_10_19/gittutoral/Data/Gaeta_etal_CLC_data.csv")
 
 data %>% 
-  mutate(length_cat = case_when((length>=200) ~ "Big", (length <= 200) ~ "Small")) -> data3
+  mutate(length_cat = case_when((length>=300) ~ "Big", (length <= 200) ~ "Small")) -> data3
 
 
