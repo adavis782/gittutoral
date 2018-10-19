5d51063b1a53a0f56fc604f6d0f88c73177ad48c
Alexa Davis <adavis31@unl.edu>
2018-10-19 11:48:38 -0500

    Take home Chall

--- fish-analysis.r
+++ fish-analysis.r
@@ -4,6 +4,9 @@ library(tidyverse)
 data<-read.csv("D:/NRES800/2018_10_19/gittutoral/Data/Gaeta_etal_CLC_data.csv")
 
 data %>% 
-  mutate(length_cat = case_when((length>=300) ~ "Big", (length <= 200) ~ "Small")) -> data3
+  mutate(length_cat = case_when((length>=200) ~ "Big", (length <= 200) ~ "Small")) -> data3
 
 
+ggplot()+
+  geom_histogram(data=data3, aes(scalelength, fill=length_cat))
++facet_wrap(~length_cat)+theme_Apres()
