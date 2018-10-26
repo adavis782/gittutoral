04a552febee9660509627fb16c187896d20b5266
Christopher Chizinski <chris.chizinski@gmail.com>
2018-10-26 13:09:48 -0500

    fixed error in data

--- fish-analysis.r
+++ fish-analysis.r
@@ -17,7 +17,7 @@ ggplot() +
 ## Chris's modifications
 
 ggplot() +
-  geom_histogram(data = fish, aes(x = scalelength, fill = length_cat), colour = "black", bins = 25) +
+  geom_histogram(data = data3, aes(x = scalelength, fill = length_cat), colour = "black", bins = 25) +
   facet_wrap(~length_cat) +
   coord_cartesian(ylim = c(0,500), xlim = c(0, 12), expand = FALSE) +
   theme_classic() +
